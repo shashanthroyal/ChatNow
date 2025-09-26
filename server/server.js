d8463358828d29a24dbe5a00fdbdf01319e289fc
bhagya-lakshmi04 <lakshmibhagya298@gmail.com>
2025-09-26 22:17:08 +0530

    added client url

--- server/server.js
+++ server/server.js
@@ -16,24 +16,8 @@ const onlineUsers = new Map();
 
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  process.env.CLIENT_URL || "https://chat-app-sayz.vercel.app",
-  "http://localhost:5173" , "https://chatnow-sayz.vercel.app/"
-];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps, curl, etc.)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use((cors))
 app.use(express.json());
 
 app.use('/api/auth',authRoutes);
